b041dc0a1e40aaebf0aacfc9612208efdf45e4ef
Kevin Kabatra <kevin@lsretail.com>
2019-11-10 20:04:13 +0000

    Beginning to add the ability for the machine to pick a number as well.

---
+++ BusinessLogic/NumberWizardBusinessLogic/Handlers/Guessing/NumberPicker.cs
@@ -0,0 +1,58 @@
+﻿namespace NumberWizardBusinessLogic.Handlers.Guessing
+{
+    using System;
+    using Display;
+
+    /// <summary>
+    ///     Random number generator for the computer to pick a number
+    /// that the user must guess.
+    /// </summary>
+    public class NumberPicker
+    {
+        private readonly int _number;
+        private readonly IDisplayHandler _displayHandler;
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="displayHandler">The handler that will display messages to the user.</param>
+        /// <param name="maximumNumber">The maximum value that the computer can pick.</param>
+        /// <param name="minimumNumber">The minimum value that the computer can pick.</param>
+        public NumberPicker(IDisplayHandler displayHandler, int maximumNumber, int minimumNumber = 0)
+        {
+            _displayHandler = displayHandler;
+            _number = GetRandomNumber(maximumNumber, minimumNumber);
+        }
+
+        /// <summary>
+        ///     Generates a random number and returns it.
+        /// </summary>
+        /// <param name="maximumNumber">The maximum value that the computer can pick.</param>
+        /// <param name="minimumNumber">The minimum value that the computer can pick.</param>
+        /// <returns>Int. Representing the number that the computer will pick.</returns>
+        private static int GetRandomNumber(int maximumNumber, int minimumNumber = 0)
+        {
+            var randomGenerator = new System.Random();
+            var randomNumber = randomGenerator.Next(minimumNumber, maximumNumber);
+
+            return randomNumber;
+        }
+
+        /// <summary>
+        ///     Validates the user's guess against the value that the computer picked.
+        /// </summary>
+        /// <param name="numberToGuess">The number that the player believes the computer has picked.</param>
+        /// <returns>Boolean. Representing if the user guess correctly or not.</returns>
+        public bool GuessNumber(int numberToGuess)
+        {
+            var success = numberToGuess == _number;
+            
+            //ToDo: set up Resource file for hard coded text
+            var successMessage = success ? "correct!" : "incorrect.";
+            var message = string.Format("Your guess was %1", successMessage);
+            _displayHandler.DisplayMessage(message);
+
+            return success;
+        }
+    }
+}
---
+++ BusinessLogic/NumberWizardBusinessLogicUnitTests/Handlers/Guessing/NumberPickerUnitTests.cs
@@ -0,0 +1,47 @@
+﻿namespace NumberWizardBusinessLogicUnitTests.Handlers.Guessing
+{
+    using Moq;
+    using NumberWizardBusinessLogic.Handlers.Guessing;
+    using NumberWizardBusinessLogic.Handlers.Display;
+    using Xunit;
+
+    /// <summary>
+    ///     Exercises the Number Picker
+    /// </summary>
+    public class NumberPickerUnitTests
+    {
+        [Fact]
+        public void CanCreateNumberPicker()
+        {
+            var displayHandler = new Mock<IDisplayHandler>().Object;
+            var numberPicker = new NumberPicker(displayHandler, 0,0);
+            
+            Assert.NotNull(numberPicker);
+        }
+
+        [Fact]
+        public void CanCreateNumberPickerWithExpectedNumber()
+        {
+            var displayHandler = new Mock<IDisplayHandler>().Object;
+            const int expectedNumber = 42;
+            var numberPicker = new NumberPicker(displayHandler, expectedNumber, expectedNumber);
+
+            var canGuessExpectedNumber = numberPicker.GuessNumber(expectedNumber);
+
+            Assert.True(canGuessExpectedNumber);
+        }
+
+        [Fact]
+        public void CanDisplayMessageToUserAfterGuess()
+        {
+            var displayHandler = new Mock<IDisplayHandler>();
+            displayHandler.Setup(mock => mock.DisplayMessage(It.IsAny<string>()));
+            const int expectedNumber = 0;
+            
+            var numberPicker = new NumberPicker(displayHandler.Object, expectedNumber);
+            numberPicker.GuessNumber(expectedNumber);
+
+            displayHandler.Verify(mock => mock.DisplayMessage(It.IsAny<string>()), Times.Once);
+        }
+    }
+}
